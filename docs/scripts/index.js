2b5e09ce008cacab78090ba75ece5b0848762119
Jan Bittendorf <80800405+JanBdf@users.noreply.github.com>
2026-03-08 12:50:34 +0100

    Cleaned up old files - movinghead.ts and movinghead.js - Corresponding code and import in index.ts

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -1,19 +1,4 @@
-import * as mh from "./lib/movinghead.js";
-function update(_ = null) {
-    let text_element = document.querySelector(".text-thingo");
-    let text_area = text_element.getBoundingClientRect();
-    mh.movingheads.forEach((movinghead) => {
-        // movinghead.pointTo((text_area.left + 0.5 * text_area.width), (text_area.top + 0.5 * text_area.height));
-    });
-}
-function onInputChange(e) {
-    let target = e.currentTarget;
-    let value = parseInt(target.value);
-    mh.movingheads.forEach((movinghead) => {
-        movinghead.setBeamAngle(value / 1000 * 90);
-    });
-}
-function toggleSidebar(e = null) {
+function toggleSidebar(_ = null) {
     let sidebar = document.querySelector(".sidebar");
     if (!sidebar.classList.contains("fade-in")) {
         sidebar.classList.add("fade-in");
@@ -26,11 +11,5 @@ function toggleSidebar(e = null) {
 }
 function main() {
     document.querySelector(".btn-sidebar").addEventListener("click", toggleSidebar);
-    // mh.init();
-    //
-    // window.addEventListener('resize', update);
-    // window.addEventListener('click', update);
-    // document.querySelector("#slider").addEventListener("input", onInputChange);
-    // update();
 }
 document.addEventListener("DOMContentLoaded", main);
--- docs/scripts/index.ts
+++ docs/scripts/index.ts
@@ -1,23 +1,4 @@
-import * as mh from "./lib/movinghead.js";
-
-function update(_: Event | null = null): void {
-    let text_element: HTMLElement = document.querySelector(".text-thingo");
-    let text_area = text_element.getBoundingClientRect();
-
-    mh.movingheads.forEach((movinghead: mh.Movinghead) => {
-        // movinghead.pointTo((text_area.left + 0.5 * text_area.width), (text_area.top + 0.5 * text_area.height));
-    });
-}
-
-function onInputChange(e: InputEvent): void {
-    let target: HTMLInputElement = e.currentTarget as HTMLInputElement;
-    let value: number = parseInt(target.value);
-    mh.movingheads.forEach((movinghead: mh.Movinghead) => {
-        movinghead.setBeamAngle(value / 1000 * 90);
-    });
-}
-
-function toggleSidebar(e: Event | null = null): void {
+function toggleSidebar(_: Event | null = null): void {
     let sidebar: HTMLElement = document.querySelector(".sidebar");
 
     if (!sidebar.classList.contains("fade-in")) {
@@ -31,15 +12,6 @@ function toggleSidebar(e: Event | null = null): void {
 
 function main(): void {
     document.querySelector(".btn-sidebar").addEventListener("click", toggleSidebar);
-
-    // mh.init();
-    //
-    // window.addEventListener('resize', update);
-    // window.addEventListener('click', update);
-
-    // document.querySelector("#slider").addEventListener("input", onInputChange);
-
-    // update();
 }
 
 document.addEventListener("DOMContentLoaded", main);
